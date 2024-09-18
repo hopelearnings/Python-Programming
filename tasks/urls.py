5601a053598481c587b5ed5b3ffe5e145fc6230c
Hopedevelopers254 <design.hopedevelopers@gmail.com>
2024-09-18 20:40:13 +0300

    add tasks updated

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = "tasks"
+
 urlpatterns = [
-      path("", views.index, name="task"),
+      path("", views.index, name="index"),
       path("add", views.add, name="add")
    ]
