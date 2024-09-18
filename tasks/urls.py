18d084af0f6e8ae95bab24e375a6ef52d0c860a2
Hopedevelopers254 <design.hopedevelopers@gmail.com>
2024-09-18 20:32:44 +0300

    new year app created and app

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path("", views.index, name="task")
+      path("", views.index, name="task"),
+      path("add", views.add, name="add")
    ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+tasks = ["Read","Sing","Pray"]
+ 
 # Create your views here.
-
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html",{
+        "tasks": tasks
+    })
+
+
+# Add to task function
+def add(request):
+    return render(request, "tasks/add.html")
