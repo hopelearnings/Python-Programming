7f74e8085e5573ebb6824e4a9f6e94f981a0f0b9
Hopedevelopers254 <design.hopedevelopers@gmail.com>
2024-09-18 17:15:36 +0300

    new year app created and app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+   return render(request, "hello/index.html")
+
+
+def hope(request):
+    return HttpResponse('Hi thre mr Hope')
+
+# def greet(request, name):
+#     return HttpResponse(f"Hi {name.capitalize()}");
+
+
+
+def greet(request, name):
+        return render(request, "hello/greet.html" ,{
+             "name": name.capitalize()
+        })
+
+  
